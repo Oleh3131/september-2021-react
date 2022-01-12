a7e97c3c1d170f9b4d6d42866e02e34c1262c04c
Oleh3131 <oshevskyioleh@gmail.com>
2022-01-12 23:19:42 +0300

    add info

--- src/commponents/SpaceX/SpaceX.js
+++ src/commponents/SpaceX/SpaceX.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import Flying from '../Flying/Flying'
 
 export default function App() {
-    let [spaceXList, setSpaceXList] = useState([]);
+    const [spaceXList, setSpaceXList] = useState([]);
 
     useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches')
@@ -11,19 +11,17 @@ export default function App() {
                 return response.json();
             })
             .then(launches => {
-                setSpaceXList(launches);
+                const flights = launches.filter(value => value.launch_year !== '2020');
+                setSpaceXList(flights);
             });
+
     }, [])
+
     return (
         <div className={'SpaceX'}>
-            {spaceXList.map(value => {
-                    if (value.launch_year !== '2020') {
 
-                        return (<Flying key={value.mission_name} mission_name={value.mission_name}
-                                        launch_year={value.launch_year} photo={value.links.mission_patch_small}/>)
-                    }
-                }
-            )}
+            {spaceXList.map(value => <Flying key={value.mission_name} mission_name={value.mission_name}
+                                             launch_year={value.launch_year} photo={value.links.mission_patch_small}/>)}
 
         </div>
 
