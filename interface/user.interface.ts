1c97d2033438252319465beee1273a2a4b5c3179
Oleh3131 <oshevskyioleh@gmail.com>
2022-02-03 17:06:17 +0300

    add info

---
+++ interface/user.interface.ts
@@ -0,0 +1,7 @@
+
+
+// export interface Iuser{
+//     name:string,
+//     age:number,
+//     status:boolean
+// }
---
+++ interface/user.interface2.ts
@@ -0,0 +1,46 @@
+
+export interface IuserTwo{
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        }
+    },
+    phone: string;
+    website: string;
+}
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// Можна розділити обєкт на дві части з вкладеністю. Хоча всерівно він буде як один
+
+
+// export interface IuserTwo{
+//     id: number;
+//     name: string;
+//     username: string;
+//     email: string;
+//     address:Iaddress;
+//     phone: string;
+//     website: string;
+// }
+//
+//
+// interface Iaddress{
+//     street: string;
+//     suite: string;
+//     city: string;
+//     zipcode: string;
+//     geo: {
+//         lat: string;
+//         lng: string;
+//     }
+// }
